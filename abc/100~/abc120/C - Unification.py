9b81f723eb3fffd023700827fced47d2dd4f2b82
malleus <veldiata.mig@gmail.com>
2020-07-01 22:16:15 +0900

    0701 training

---
+++ abc/100~/abc120/C - Unification.py
@@ -0,0 +1,10 @@
+s=input()
+
+a=s.count('0')
+b=s.count('1')
+
+if a>=b:
+    print(b*2)
+
+else:
+    print(a*2)
---
+++ abc/1~99/abc092/A - Traveling Budget.py
@@ -0,0 +1,3 @@
+l=[int(input()) for i in range(4)]
+
+print(min(l[0],l[1])+min(l[2],l[3]))
