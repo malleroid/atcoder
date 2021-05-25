a65325445b50e920cebebefbd5528c7d4751891c
malleroid <veldiata.mig@gmail.com>
2021-05-25 15:14:06 +0000

    0526

--- abc/abc095/c/c.py
+++ abc/abc095/c/c.py
@@ -1,1 +1,17 @@
 A, B, C, X, Y = map(int, input().split())
+
+ans = 0
+
+if A+B <= 2*C:
+    ans = A*X+B*Y
+
+else:
+    ans += 2*C*min(X, Y)
+
+    if X >= Y:
+        ans += min((X-Y)*2*C, (X-Y)*A)
+
+    else:
+        ans += min((Y-X)*2*C, (Y-X)*B)
+
+print(ans)
