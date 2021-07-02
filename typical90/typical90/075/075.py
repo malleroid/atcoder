3a6b10b9384924708cd50c57a391c3751ba1ca88
malleroid <veldiata.mig@gmail.com>
2021-07-02 05:32:00 +0000

    0702

--- typical90/typical90/075/075.py
+++ typical90/typical90/075/075.py
@@ -1,1 +1,29 @@
+import copy
+
 N = int(input())
+
+A = [N]
+ans = 0
+
+while len(A) > 0:
+
+    B = []
+    for v in A:
+
+        i = 1
+        p = 1
+        while i*i <= v:
+            if v % i == 0:
+                p = i
+
+            i += 1
+
+        if p != 1:
+            B.append(p)
+            B.append(v//p)
+
+    A = copy.deepcopy(B)
+    if len(A) > 0:
+        ans += 1
+
+print(ans)
