e36c5358462eca4e4c278702942a3a15c04ac2e9
malleroid <veldiata.mig@gmail.com>
2021-08-17 14:50:42 +0000

    0817

--- abc/abc128/c/c.py
+++ abc/abc128/c/c.py
@@ -4,6 +4,28 @@ N, M = map(int, input().split())
 ks = [list(map(int, input().split())) for _ in range(M)]
 p = list(map(int, input().split()))
 
+k = [ks[i][0] for i in range(M)]
+s = [ks[i][1:] for i in range(M)]
+
+n = [i+1 for i in range(N)]
+
+ans = 0
 for v in itertools.product([False, True], repeat=N):
+    cnt_flag = True
+    v = list(v)
+    m = [x*y for x, y in zip(n, v)]
 
     for i in range(M):
+        on_cnt = 0
+        for j in range(k[i]):
+            if m[s[i][j]-1] != 0:
+                on_cnt += 1
+
+        if on_cnt % 2 != p[i]:
+            cnt_flag = False
+            break
+
+    if cnt_flag is True:
+        ans += 1
+
+print(ans)
--- typical90/typical90/022/022.py
+++ typical90/typical90/022/022.py
@@ -1,11 +1,8 @@
-# 最大公約数(本来はユークリッドの互除法)
 import math
 
 A, B, C = map(int, input().split())
 
-gcd_ab = math.gcd(A, B)
-gcd = math.gcd(gcd_ab, C)
+gcd = math.gcd(A, B)
+gcd = math.gcd(gcd, C)
 
-ans = A//gcd+B//gcd+C//gcd-3
-
-print(ans)
+print((A+B+C)//gcd-3)
