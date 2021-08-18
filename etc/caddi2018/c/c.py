b86d9668c3962092fbbd2a7289db815372016038
malleroid <veldiata.mig@gmail.com>
2021-08-18 15:42:34 +0000

    0818

--- etc/caddi2018/c/c.py
+++ etc/caddi2018/c/c.py
@@ -1,1 +1,19 @@
 N, P = map(int, input().split())
+
+if N >= 41:
+    ans = 1
+
+elif N == 1:
+    ans = P
+
+else:
+    ans = 1
+    i = 1
+    while i**N <= P:
+
+        if P % i**N == 0:
+            ans = i
+
+        i += 1
+
+print(ans)
