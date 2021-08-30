e02bb411ce38135e9a2a96e1b727d54034915e7c
malleroid <veldiata.mig@gmail.com>
2021-08-30 14:12:19 +0000

    0830

--- abc/abc216/d/d.py
+++ abc/abc216/d/d.py
@@ -2,11 +2,36 @@ import collections
 
 N, M = map(int, input().split())
 
-k = [0]*M
 a = [0]*M
-for _ in range(M):
-    k[_] = int(input())
-    a[_] = collections.deque(map(int, input().split()))
+cnt = [[] for _ in range(N)]
+q = collections.deque()
+for i in range(M):
+    k = int(input())
+    ai = list(map(int, input().split()))
 
-# print(k)
-# print(a)
+    aq = collections.deque()
+    for j in range(k):
+        aq.append(ai[j]-1)
+
+    a[i] = aq
+    cnt[aq[0]].append(i)
+    if len(cnt[aq[0]]) == 2:
+        q.append(aq[0])
+
+while q:
+    n = q.pop()
+    for i in range(2):
+        index = cnt[n][i]
+        a[index].popleft()
+        if len(a[index]) != 0:
+            num = a[index][0]
+            cnt[num].append(index)
+            if len(cnt[num]) == 2:
+                q.append(num)
+
+for i in range(len(a)):
+    if len(a[i]) != 0:
+        print('No')
+        exit()
+
+print('Yes')
