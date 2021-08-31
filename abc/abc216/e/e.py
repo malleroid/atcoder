92200d1ec01f948fe2a924a8f5eae34b7b335db0
malleroid <veldiata.mig@gmail.com>
2021-08-31 14:57:09 +0000

    0831

--- abc/abc216/e/e.py
+++ abc/abc216/e/e.py
@@ -1,2 +1,29 @@
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
+
+s = sum(A)
+
+ans = 0
+if K >= s:
+    for i in range(N):
+        ans += A[i]*(A[i]+1)//2
+
+else:
+    A.sort(reverse=True)
+    A.append(0)
+
+    for i in range(N):
+
+        dif = A[i]-A[i+1]
+        if dif*(i+1) <= K:
+            ans += (A[i]*(A[i]+1)//2-A[i+1]*(A[i+1]+1)//2)*(i+1)
+            K -= dif*(i+1)
+
+        else:
+            q = K//(i+1)
+            r = K % (i+1)
+            ans += (A[i]*(A[i]+1)//2-(A[i]-q)*(A[i]-q+1)//2)*(i+1)
+            ans += (A[i]-q)*r
+            break
+
+print(ans)
