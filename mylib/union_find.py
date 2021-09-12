ba4cf496c1ca698c3775f3177c0af11a75f848c1
malleroid <veldiata.mig@gmail.com>
2021-09-12 09:24:38 +0000

    0912

--- mylib/union_find.py
+++ mylib/union_find.py
@@ -1,7 +1,5 @@
-import sys
-sys.setrecursionlimit(10**6)
+# ランクなしシンプル版
 
-N, M = map(int, input().split())
 parents = [-1]*N
 
 
@@ -28,28 +26,3 @@ def unite(x, y):
 
 def same(x, y):
     return find(x) == find(y)
-
-
-paths = []
-for i in range(M):
-    A, B, C = map(int, input().split())
-    A -= 1
-    B -= 1
-
-    if A == B and C < 0:
-        continue
-
-    paths.append([C, A, B])
-
-paths.sort()
-
-ans = 0
-for path in paths:
-    c, a, b = path[0], path[1], path[2]
-    if same(a, b) and c > 0:
-        ans += c
-
-    else:
-        unite(a, b)
-
-print(ans)
