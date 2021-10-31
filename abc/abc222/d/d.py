4f32efe996785b58a9b4e3ad2e038d2672591fcd
malleroid <veldiata.mig@gmail.com>
2021-10-31 10:17:51 +0000

    1031

--- abc/abc222/d/d.py
+++ abc/abc222/d/d.py
@@ -4,22 +4,18 @@ b = list(map(int, input().split()))
 
 mod = 998244353
 
-dp = [0]*N
+dp = [[0]*3001 for _ in range(N+1)]
 
-floor = a[0]
-ceil = b[0]
+for i in range(3001):
+    dp[0][i] = 1
 
-dp[0] = ceil-floor+1
-for i in range(1, N):
-    # dp[i] = dp[i-1]
-    for j in range(floor, ceil+1):
-        p = max(j, a[i])
-        q = b[i]
-
-        dp[i] += q-p+1
-        # print(p, q, dp)
+for i in range(N):
     floor = a[i]
     ceil = b[i]
-    dp[i] %= mod
+    for j in range(3001):
+        if floor <= j <= ceil:
+            dp[i+1][j] = (dp[i][j]+dp[i+1][max(0, j-1)]) % mod
+        else:
+            dp[i+1][j] = dp[i+1][max(0, j-1)]
 
-print(dp[N-1])
+print(dp[-1][-1])
