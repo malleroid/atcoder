2ec7cdb6a0416c8f8bd39059d017605995fd7ab1
malleroid <veldiata.mig@gmail.com>
2022-01-09 14:44:48 +0000

    0109

--- abc/abc234/d/d.py
+++ abc/abc234/d/d.py
@@ -1,16 +1,14 @@
-import bisect
-
+import heapq
 N, K = map(int, input().split())
 P = list(map(int, input().split()))
 
 p = P[:K]
-p.sort()
 
+heapq.heapify(p)
 ans = [p[-K]]*(N-K+1)
-
 for i in range(N-K):
     n = P[K+i]
-    bisect.insort(p, n)
+    heapq.heappushpop(p, n)
     ans[i+1] = p[-K]
 
 print(*ans, sep='\n')
