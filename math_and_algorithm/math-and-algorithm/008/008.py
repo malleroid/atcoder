672e9d754de5d7abe2ce182ff7b3981f614ba06d
malleroid <veldiata.mig@gmail.com>
2022-02-08 13:52:01 +0000

    0208

---
+++ math_and_algorithm/math-and-algorithm/008/008.py
@@ -0,0 +1,11 @@
+N, S = map(int, input().split())
+
+ans = 0
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        if i+j <= S:
+            ans += 1
+        else:
+            break
+
+print(ans)
